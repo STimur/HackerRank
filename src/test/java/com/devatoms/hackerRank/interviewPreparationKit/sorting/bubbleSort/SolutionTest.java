b7493137672281686875826f7cf43a7df7877714
tim <timur.salakhetdinov@gmail.com>
2019-12-23 19:54:38 +0300

    init commit

---
+++ src/test/java/com/devatoms/hackerRank/interviewPreparationKit/sorting/bubbleSort/SolutionTest.java
@@ -0,0 +1,14 @@
+package com.devatoms.hackerRank.interviewPreparationKit.sorting.bubbleSort;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SolutionTest {
+
+  @Test
+  public void dummy() {
+    assertThat(1 + 1, is(2));
+  }
+}
