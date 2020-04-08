7ac17b53b9bff75fe2de1954e7d82ed93f080263
STimur <timur.salakhetdinov@gmail.com>
2020-04-08 11:33:07 +0300

    add one element test

--- src/test/java/com/devatoms/hackerRank/interviewPreparationKit/linkedLists/reverseDoublyLinkedList/SolutionTest.java
+++ src/test/java/com/devatoms/hackerRank/interviewPreparationKit/linkedLists/reverseDoublyLinkedList/SolutionTest.java
@@ -2,19 +2,44 @@ package com.devatoms.hackerRank.interviewPreparationKit.linkedLists.reverseDoubl
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 public class SolutionTest {
 
+	private void assertDoubleLinkedListIs(DoublyLinkedListNode head, List<Integer> ints) {
+		DoublyLinkedListNode currentNode = head;
+		int i = 0;
+		for (; currentNode != null; i++, currentNode = currentNode.next) {
+			assertThat(currentNode.data, is(ints.get(i)));
+		}
+		assertThat(i, is(ints.size()));
+	}
+
+	private List<Integer> intList(Integer... ints) {
+		return Arrays.asList(ints);
+	}
+
 	@Test
 	public void null_list() {
-		assertThat(reverse(null), is(nullValue()));
+		DoublyLinkedList list = new DoublyLinkedList();
+		assertDoubleLinkedListIs(reverse(list.head), Collections.emptyList());
+	}
+
+	@Test
+	public void one_element_list() {
+		DoublyLinkedList list = new DoublyLinkedList();
+		list.insertNode(1);
+		assertDoubleLinkedListIs(reverse(list.head), intList(1));
 	}
 
 	static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
-		return null;
+		return head;
 	}
 
 	static class DoublyLinkedListNode {
