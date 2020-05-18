8861aa43091c1d5a3ffe48ccc556342c6a10fb1e
tim <timur.salakhetdinov@gmail.com>
2020-05-18 16:45:06 +0300

    solved new task

--- src/test/java/com/devatoms/hackerRank/algorithms/strings/gameOfThronesOne/SolutionTest.java
+++ src/test/java/com/devatoms/hackerRank/algorithms/strings/gameOfThronesOne/SolutionTest.java
@@ -2,20 +2,42 @@ package com.devatoms.hackerRank.algorithms.strings.gameOfThronesOne;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SolutionTest {
 
 	@Test
-	public void solution() {
+	public void solution() throws IOException {
 		assertThat(gameOfThrones("a"), is("YES"));
 		assertThat(gameOfThrones("ab"), is("NO"));
+		assertThat(gameOfThrones("aba"), is("YES"));
+		assertThat(gameOfThrones("aabb"), is("YES"));
 	}
 
 	static String gameOfThrones(String s) {
-		if (s.length() == 1)
-			return "YES";
-		return "NO";
+		Map<Character, Integer> dict = new HashMap<>();
+
+		for (char c : s.toCharArray()) {
+			if (dict.containsKey(c))
+				dict.put(c, dict.get(c) + 1);
+			else
+				dict.put(c, 1);
+		}
+
+		int numberOfOddChars = 0;
+		for (int i : dict.values())
+			if (i % 2 == 1)
+				numberOfOddChars++;
+
+		if (numberOfOddChars > 1)
+			return "NO";
+		return "YES";
 	}
 }
