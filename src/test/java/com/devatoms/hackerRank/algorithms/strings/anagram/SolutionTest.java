14ccae7ced81a6cce34ba24442ec2c5432e60405
tim <timur.salakhetdinov@gmail.com>
2020-05-19 15:55:06 +0300

    started new task

---
+++ src/test/java/com/devatoms/hackerRank/algorithms/strings/anagram/SolutionTest.java
@@ -0,0 +1,18 @@
+package com.devatoms.hackerRank.algorithms.strings.anagram;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SolutionTest {
+
+	@Test
+	public void solution() {
+		assertThat(anagram("a"), is(0));
+	}
+
+	int anagram(String s) {
+		return 0;
+	}
+}
