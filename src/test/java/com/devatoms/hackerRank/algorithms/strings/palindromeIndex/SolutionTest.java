f9ce76ca11388e744e74b17e90ba2dd1c7b012c8
tim <timur.salakhetdinov@gmail.com>
2020-05-20 15:32:46 +0300

    it works but not fast enough

--- src/test/java/com/devatoms/hackerRank/algorithms/strings/palindromeIndex/SolutionTest.java
+++ src/test/java/com/devatoms/hackerRank/algorithms/strings/palindromeIndex/SolutionTest.java
@@ -10,9 +10,26 @@ public class SolutionTest {
 	@Test
 	public void solution() {
 		assertThat(palindromeIndex("a"), is(-1));
+		assertThat(palindromeIndex("ab"), is(0));
+		assertThat(palindromeIndex("aba"), is(-1));
+	}
+
+	@Test
+	public void string_builder() {
+		assertThat(new StringBuilder("ab").reverse().toString(), is("ba"));
+		assertThat(new StringBuilder("ab").deleteCharAt(0).toString(), is("b"));
 	}
 
 	static int palindromeIndex(String s) {
+		if (s.length() == 1 || s.equals(new StringBuilder(s).reverse().toString()))
+			return -1;
+
+		for (int i = 0; i < s.length(); i++) {
+			StringBuilder forward = new StringBuilder(s).deleteCharAt(i);
+			StringBuilder reverse = new StringBuilder(forward.toString()).reverse();
+			if (forward.toString().equals(reverse.toString()))
+				return i;
+		}
 		return -1;
 	}
 }
