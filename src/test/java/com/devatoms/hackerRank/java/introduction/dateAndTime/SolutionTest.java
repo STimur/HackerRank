bff3adaa90e9c54305742ebab543f4040736a203
tim <timur.salakhetdinov@gmail.com>
2020-05-21 19:36:53 +0300

    removed unnecessary comment

--- src/test/java/com/devatoms/hackerRank/java/introduction/dateAndTime/SolutionTest.java
+++ src/test/java/com/devatoms/hackerRank/java/introduction/dateAndTime/SolutionTest.java
@@ -24,16 +24,6 @@ public class SolutionTest {
 		assertThat(Calendar.MONDAY, is(2));
 	}
 
-	/*
-	 * Complete the 'findDay' function below.
-	 *
-	 * The function is expected to return a STRING.
-	 * The function accepts following parameters:
-	 *  1. INTEGER month
-	 *  2. INTEGER day
-	 *  3. INTEGER year
-	 */
-
 	public static String findDay(int month, int day, int year) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(year, month - 1, day);
