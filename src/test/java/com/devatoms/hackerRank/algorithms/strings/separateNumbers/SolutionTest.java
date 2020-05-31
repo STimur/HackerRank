972e60d8dec6b768245c3886bf53b17446b3749b
tim <timur.salakhetdinov@gmail.com>
2020-05-31 17:24:35 +0300

    solved task a bit

--- src/test/java/com/devatoms/hackerRank/algorithms/strings/separateNumbers/SolutionTest.java
+++ src/test/java/com/devatoms/hackerRank/algorithms/strings/separateNumbers/SolutionTest.java
@@ -25,52 +25,45 @@ public class SolutionTest {
 	}
 
 	@Test
-	public void ancillary() {
-		assertThat(isSeparatable("1", 1)).isEqualTo(true);
-		assertThat(isSeparatable("13", 1)).isEqualTo(false);
-		assertThat(isSeparatable("1011", 2)).isEqualTo(true);
-		assertThat(isSeparatable("910", 2)).isEqualTo(true);
-	}
-
-	@Test
 	public void solution() {
 		separateNumbers("1");
-		assertThat(outContent.toString()).isEqualTo("NO");
+		assertThat(outContent.toString()).isEqualTo("NO\n");
 		outContent.reset();
 		separateNumbers("12");
-		assertThat(outContent.toString()).isEqualTo("YES");
+		assertThat(outContent.toString()).isEqualTo("YES 1\n");
 		outContent.reset();
 		separateNumbers("124");
-		assertThat(outContent.toString()).isEqualTo("NO");
+		assertThat(outContent.toString()).isEqualTo("NO\n");
 		outContent.reset();
 		separateNumbers("1011");
-		assertThat(outContent.toString()).isEqualTo("YES");
+		assertThat(outContent.toString()).isEqualTo("YES 10\n");
+		outContent.reset();
+		separateNumbers("91011");
+		assertThat(outContent.toString()).isEqualTo("YES 9\n");
 	}
 
 	static void separateNumbers(String s) {
-		if (s.length() == 1) {
-			System.out.print("NO");
-			return;
-		}
 
-		int numOfCharsInInt = 1;
-		for (int i = 0; i < s.length() - 1; i++) {
-			if (s.charAt(i + 1) - s.charAt(i) != 1) {
-				System.out.print("NO");
-				return;
+		for (int numOfCharsInInt = 1; numOfCharsInInt <= s.length() / 2; numOfCharsInInt++) {
+			final String first = s.substring(0, numOfCharsInInt);
+
+			for (int i = 0; i < s.length() - numOfCharsInInt; ) {
+				final Integer cur = Integer.valueOf(s.substring(i, i + numOfCharsInInt));
+				final String next = String.valueOf(cur + 1);
+
+				if (!s.substring(i + numOfCharsInInt, i + numOfCharsInInt + next.length()).equals(next)) {
+					break;
+				}
+
+				if (i == s.length() - numOfCharsInInt - next.length()) {
+					System.out.println("YES " + first);
+					return;
+				}
+
+				i += numOfCharsInInt;
+				numOfCharsInInt = next.length();
 			}
 		}
-		System.out.print("YES");
-	}
-
-	static boolean isSeparatable(String s, int numOfCharsInInt) {
-		for (int i = 0; i < s.length() - numOfCharsInInt; i += numOfCharsInInt) {
-			final Integer Int = Integer.valueOf(s.substring(i, i + numOfCharsInInt));
-			final Integer nextInt = Integer.valueOf(s.substring(i + numOfCharsInInt, i + 2 * numOfCharsInInt));
-			if (nextInt - Int != 1) {
-				return false;
-			}
-		}
-		return true;
+		System.out.println("NO");
 	}
 }
