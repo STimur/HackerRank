80a62de0ba3efc6d59bc8bf2cb0f5e5316edfe0c
tim <timur.salakhetdinov@gmail.com>
2021-02-09 22:44:45 +0300

    solved countDuplets subproblem

--- src/test/java/com/devatoms/hackerRank/interviewPreparationKit/hashmaps/countTriplets/SolutionTest.java
+++ src/test/java/com/devatoms/hackerRank/interviewPreparationKit/hashmaps/countTriplets/SolutionTest.java
@@ -3,23 +3,49 @@ package com.devatoms.hackerRank.interviewPreparationKit.hashmaps.countTriplets;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
 
 	@Test
+	public void ancillary() {
+		assertThat(countDuplets(Arrays.asList(1L), 1)).isEqualTo(0);
+		assertThat(countDuplets(Arrays.asList(1L, 2L), 2)).isEqualTo(1);
+		assertThat(countDuplets(Arrays.asList(1L, 1L, 2L), 2)).isEqualTo(2);
+		assertThat(countDuplets(Arrays.asList(1L, 1L, 2L, 2L), 2)).isEqualTo(4);
+		assertThat(countDuplets(Arrays.asList(1L, 1L, 1L), 1)).isEqualTo(3);
+	}
+
+	@Test
 	public void solution() {
 		assertThat(countTriplets(Arrays.asList(1L), 1)).isEqualTo(0);
 		assertThat(countTriplets(Arrays.asList(1L, 1L, 1L), 1)).isEqualTo(1);
-		assertThat(countTriplets(Arrays.asList(1L, 1L, 1L), 2)).isEqualTo(0);
+		assertThat(countTriplets(Arrays.asList(1L, 2L, 4L), 2)).isEqualTo(1);
 	}
 
 	static long countTriplets(List<Long> arr, long r) {
 		if (arr.size() < 3)
 			return 0;
 
-		return (arr.get(0) == r * arr.get(1) && arr.get(1) == r * arr.get(2)) ? 1 : 0;
+		return (r * arr.get(0) == arr.get(1) && r * arr.get(1) == arr.get(2)) ? 1 : 0;
+	}
+
+	private long countDuplets(List<Long> arr, int r) {
+		Map<Long, Long> numOfFirstElementsFor = new HashMap<>();
+		long numOfDuplets = 0;
+
+		for (long n : arr) {
+			Long numOfNewDuplets = numOfFirstElementsFor.get(n);
+			if (numOfNewDuplets != null)
+				numOfDuplets += numOfNewDuplets;
+
+			numOfFirstElementsFor.compute(n * r, (k, v) -> v == null ? 1 : v + 1);
+		}
+
+		return numOfDuplets;
 	}
 }
