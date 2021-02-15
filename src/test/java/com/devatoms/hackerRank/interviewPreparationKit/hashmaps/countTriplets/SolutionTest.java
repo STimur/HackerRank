f6b2bbd0755234d86c7c3dcad6740b2b4bfeb9f6
tim <timur.salakhetdinov@gmail.com>
2021-02-16 00:16:04 +0300

    solved countTriplets problem

--- src/test/java/com/devatoms/hackerRank/interviewPreparationKit/hashmaps/countTriplets/SolutionTest.java
+++ src/test/java/com/devatoms/hackerRank/interviewPreparationKit/hashmaps/countTriplets/SolutionTest.java
@@ -16,12 +16,26 @@ public class SolutionTest {
 		assertThat(countTriplets(Arrays.asList(1L), 1)).isEqualTo(0);
 		assertThat(countTriplets(Arrays.asList(1L, 2L, 4L), 2)).isEqualTo(1);
 		assertThat(countTriplets(Arrays.asList(1L, 2L, 4L), 3)).isEqualTo(0);
+		assertThat(countTriplets(Arrays.asList(1L, 1L, 1L, 1L, 1L), 1)).isEqualTo(10);
 	}
 
 	static long countTriplets(List<Long> arr, long r) {
-		if (arr.size() < 3)
-			return 0;
+		Map<Long, Long> numOfFirstElementsFor = new HashMap<>();
+		Map<Long, Long> numOfDupletsFor = new HashMap<>();
+		long numOfTriplets = 0;
 
-		return ((r * arr.get(0) == arr.get(1)) && (r * arr.get(1) == arr.get(2))) ? 1 : 0;
+		for (long n : arr) {
+			final Long numOfNewTriplets = numOfDupletsFor.get(n);
+			if (numOfNewTriplets != null)
+				numOfTriplets += numOfNewTriplets;
+
+			final Long numOfNewDuplets = numOfFirstElementsFor.get(n);
+			if (numOfNewDuplets != null)
+				numOfDupletsFor.compute(n * r, (k, v) -> v == null ? numOfNewDuplets : v + numOfNewDuplets);
+
+			numOfFirstElementsFor.compute(n * r, (k, v) -> v == null ? 1 : v + 1);
+		}
+
+		return numOfTriplets;
 	}
 }
